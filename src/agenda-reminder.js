74ea2c1b450c6683dcc924fa0b696cc9b09902ea
José Silva <juniornyanata@gmail.com>
2026-09-02 02:28:31 -0300

    feat: mudança na leitura do excel13

--- src/agenda-reminder.js
+++ src/agenda-reminder.js
@@ -130,7 +130,7 @@ function formatReminderMessage(appointment) {
   }).format(start);
 
   return [
-    'Agenda:',
+    'AGENDA:',
     `${appointment.title}`,
     `${date} às ${time}`,
     `Origem: ${appointment.source}`
